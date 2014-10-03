8b703d4771035e4af46eaec4abe1829fc2c39d7a
Dmitry Kamorin <dmitry.kamorin@aldigit.com>
2014-10-03 14:10:47 +0700

    Add ejs-locals

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var articles = require('routes/articles');
 var app = express();
 
 // view engine setup
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
